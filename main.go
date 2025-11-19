b25288856d7e7bbbf76e966320132175074901e9
default <nicholas.sun@stu.ocsb.ca>
2025-11-19 03:31:16 +0000

    Added commented TypeScript and Go programs.

---
+++ main.go
@@ -0,0 +1,42 @@
+/**
+ * @author Nicholas Sun
+ * @version 1.0.0
+ * @date 2025-11-18
+ * @fileoverview This program prints the multiplication table from 0 to 12 with the number 9.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring variables
+	var num1 int = 0 * 9
+	var num2 int = 1 * 9
+	var num3 int = 2 * 9
+	var num4 int = 3 * 9
+	var num5 int = 4 * 9
+	var num6 int = 5 * 9
+	var num7 int = 6 * 9
+	var num8 int = 7 * 9
+	var num9 int = 8 * 9
+	var num10 int = 9 * 9
+	var num11 int = 10 * 9
+	var num12 int = 11 * 9
+	var num13 int = 12 * 9
+
+	// Printing
+	fmt.Println("0 * 9 = " + fmt.Sprint(num1))
+	fmt.Println("1 * 9 = " + fmt.Sprint(num2))
+	fmt.Println("2 * 9 = " + fmt.Sprint(num3))
+	fmt.Println("3 * 9 = " + fmt.Sprint(num4))
+	fmt.Println("4 * 9 = " + fmt.Sprint(num5))
+	fmt.Println("5 * 9 = " + fmt.Sprint(num6))
+	fmt.Println("6 * 9 = " + fmt.Sprint(num7))
+	fmt.Println("7 * 9 = " + fmt.Sprint(num8))
+	fmt.Println("8 * 9 = " + fmt.Sprint(num9))
+	fmt.Println("9 * 9 = " + fmt.Sprint(num10))
+	fmt.Println("10 * 9 = " + fmt.Sprint(num11))
+	fmt.Println("11 * 9 = " + fmt.Sprint(num12))
+	fmt.Println("12 * 9 = " + fmt.Sprint(num13))
+}
---
+++ main.ts
@@ -0,0 +1,36 @@
+/**
+ * @author Nicholas Sun
+ * @version 1.0.0
+ * @date 2025-11-18
+ * @fileoverview This program prints the multiplication table from 0 to 12 with the number 9.
+ */
+
+// Declaring variables
+var num1: number = 0 * 9;
+var num2: number = 1 * 9;
+var num3: number = 2 * 9;
+var num4: number = 3 * 9;
+var num5: number = 4 * 9;
+var num6: number = 5 * 9;
+var num7: number = 6 * 9;
+var num8: number = 7 * 9;
+var num9: number = 8 * 9;
+var num10: number = 9 * 9;
+var num11: number = 10 * 9;
+var num12: number = 11 * 9;
+var num13: number = 12 * 9;
+
+// Printing
+console.log("0 * 9 = " + String(num1));
+console.log("1 * 9 = " + String(num2));
+console.log("2 * 9 = " + String(num3));
+console.log("3 * 9 = " + String(num4));
+console.log("4 * 9 = " + String(num5));
+console.log("5 * 9 = " + String(num6));
+console.log("6 * 9 = " + String(num7));
+console.log("7 * 9 = " + String(num8));
+console.log("8 * 9 = " + String(num9));
+console.log("9 * 9 = " + String(num10));
+console.log("10 * 9 = " + String(num11));
+console.log("11 * 9 = " + String(num12));
+console.log("12 * 9 = " + String(num13));
